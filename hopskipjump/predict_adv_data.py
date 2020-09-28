f06c1920731192ac971ee524e3fc951634f56930
Meiyan Xie <meiyan@blupig.net>
2020-09-28 10:18:33 -0700

    Compute diversity of test[1000]

--- hopskipjump/predict_adv_data.py
+++ hopskipjump/predict_adv_data.py
@@ -11,17 +11,17 @@ from core.bnn import *
 def main():
 
     # # Define variable
-    # modelpath = '../binary/checkpoints/cifar10_scdcemlpbnn_100_br02_h20_nr075_ni10000_i1_0.pkl'
-    # print('\n------------- model -------------\n', modelpath)
+    modelpath = '../binary/checkpoints/cifar10_scdcemlpbnn_100_br02_h20_nr075_ni10000_i1_0.pkl'
+    print('\n------------- model -------------\n', modelpath)
 
     # Load model
-    # with open(modelpath, 'rb') as f:
-    #     model = pickle.load(f)
+    with open(modelpath, 'rb') as f:
+        model = pickle.load(f)
 
-    model = BNN(['../binary/checkpoints/cifar10_mlpbnn_approx_%d.h5' % (i) for i in range(100)])
-    print('------------- model -------------\n', 'cifar10_mlpbnn_approx')
+    # model = BNN(['../binary/checkpoints/cifar10_mlpbnn_approx_%d.h5' % (i) for i in range(100)])
+    # print('------------- model -------------\n', 'cifar10_mlpbnn_approx')
 
-    adv_data = np.load('adv_data/bnn_adv_data_6.npy')
+    adv_data = np.load('scdcemlp_bnn_adv_data_1000.npy')
     print('adv shape', adv_data.shape)
     # adv_data = adv_data.reshape(-1, 3, 32, 32)
     # print('adv shape', adv_data.shape)
@@ -33,14 +33,14 @@ def main():
         print('=======================')
         print('\nVote id: {}\n'.format(vote))
         misclassified_count = 0
-        pred_y = model.predict(adv_data, best_index=vote).astype(int)
-        # pred_y = model.predict(adv_data, kind='best', best_index=vote).astype(int)
+        # pred_y = model.predict(adv_data, best_index=vote).astype(int)
+        pred_y = model.predict(adv_data, kind='best', best_index=vote).astype(int)
         # pred_y = model.estimators_[vote].predict(adv_data)
 
-        # The true label is 1
+        # The true label is 0
         print('pred_y', pred_y)
         for idx in range(len(pred_y)):
-            if pred_y[idx] == 0:
+            if pred_y[idx] == 1:
                 misclassified_count += 1
 
         print('Vote {} misclassified count is {}\n'.format(vote, misclassified_count))
